13c491549254d6fa5b76e8a837459599ac97c1cb
Alex Huang <hwangzhiming@gmail.com>
2019-10-16 09:36:37 +0800

    add js support

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,15 @@ const {src, dest, parallel, watch, series} = require('gulp'),
     ejs = require('gulp-ejs'),
     browserSync = require('browser-sync').create();
 
-
+// compile html
 function html() {
     return src('src/*.ejs')
         .pipe(ejs({}))
-        .pipe(rename({ extname: '.html' }))
+        .pipe(rename({extname: '.html'}))
         .pipe(dest('dist/'))
 }
 
+// compile scss
 function scss() {
     return src('src/assets/scss/*.scss')
         .pipe(sass({outputStyle: 'expanded'}))
@@ -40,6 +41,7 @@ function scss() {
         .pipe(dest('dist/assets/css'))
 }
 
+// serve and live reload
 function serve() {
     browserSync.init({
         files: "./*.html",
@@ -58,21 +60,53 @@ function serve() {
     })
 }
 
+// clean build folder
 async function clean() {
     await del(['dist'])
 }
 
-// function js() {
-//     return src('client/javascript/*.js', {sourcemaps: true})
-//         .pipe(concat('app.min.js'))
-//         .pipe(dest('build/js', {sourcemaps: true}))
-// }
-exports.build = series(clean, parallel(scss, html));
+// copy asset files
+async function copyFiles() {
+    return src(['src/assets/**/*.*', '!src/assets/scss/**/*.*'])
+        .pipe(dest('dist/assets'))
+}
 
+// copy bootstrap to vendor folder
+async function bootstrap() {
+    return src(['node_modules/bootstrap/dist/**/*.*'])
+        .pipe(dest('src/assets/vendor/bootstrap'))
+}
+
+// copy popper.js to vendor folder
+async function popperJs() {
+    return src(['node_modules/popper.js/dist/umd/**/*.*'])
+        .pipe(dest('src/assets/vendor/popper.js'))
+}
+
+// copy jquery to vendor folder
+async function jquery() {
+    return src(['node_modules/jquery/dist/**/*.*'])
+        .pipe(dest('src/assets/vendor/jquery'))
+}
+
+// clean default vendors
+async function cleanDefaultVendors() {
+    await del(['src/assets/vendor/bootstrap', 'src/assets/vendor/popper.js', 'src/assets/vendor/jquery']);
+}
+
+// build and publish
+exports.build = series(clean, parallel(copyFiles, scss, html));
+
+// default task for deving
 exports.default = function () {
     serve();
     watch('src/**/*.ejs', {ignoreInitial: false}, html);
     watch('src/assets/scss/**/*.scss', {ignoreInitial: false}, scss);
+    watch(['src/assets/**/*.*', '!src/assets/scss/**/*.*'], {ignoreInitial: false}, copyFiles);
     watch('dist/**/*.*').on('change', browserSync.reload);
 };
+
+// clean default vendors and copy them to vendor folders
+// trigger by using the postinstall script of package.json
+exports.vendors = series(cleanDefaultVendors, bootstrap, popperJs, jquery);
 
